f692af2c63020a3896ec8c2a4598751c9c9bedf2
Christian Goldapp <c.goldapp@tu-bs.de>
2018-08-14 00:12:25 +0200

    Kotlinify

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,6 @@
 import assembler.Assembler
-import assembler.AssemblyException
 import common.Word32
-
 import java.io.File
-import java.io.IOException
 import java.nio.charset.Charset
 import java.nio.file.Files
 
@@ -11,43 +8,35 @@ import java.nio.file.Files
  * @author Chris Gold
  * @version 1.0
  */
-internal object Main {
-    @Throws(AssemblyException::class, IOException::class)
-    @JvmStatic
-    fun main(args: Array<String>) {
-        assembleAndExecute(File(args[0]))
-    }
+fun main(args: Array<String>) {
+    assembleAndExecute(File(args[0]))
+}
 
-    @Throws(IOException::class)
-    private fun readWordsFromFile(f: File): Array<Word32> {
-        val b = Files.readAllBytes(f.toPath())
-        return Word32.bytesToWords(b)
-    }
+private fun readWordsFromFile(f: File): Array<Word32> {
+    val b = Files.readAllBytes(f.toPath())
+    return Word32.bytesToWords(b)
+}
 
-    @Throws(IOException::class)
-    private fun execute(f: File) {
-        val program = readWordsFromFile(f)
-        execute(program)
-    }
+private fun execute(f: File) {
+    val program = readWordsFromFile(f)
+    execute(program)
+}
 
-    @Throws(IOException::class, AssemblyException::class)
-    private fun assembleToFile(f: File) {
-        val assembly = f.name.split(".".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0] + ".pro"
-        val lines = Files.readAllLines(f.toPath(), Charset.defaultCharset())
-        val program = Assembler.assemble(Assembler.parse(lines.toTypedArray()))
-    }
+private fun assembleToFile(f: File) {
+    val assembly = f.name.split(".".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0] + ".pro"
+    val lines = Files.readAllLines(f.toPath(), Charset.defaultCharset())
+    val program = Assembler.assemble(Assembler.parse(lines.toTypedArray()))
+}
 
-    @Throws(IOException::class, AssemblyException::class)
-    private fun assembleAndExecute(f: File) {
-        val lines = Files.readAllLines(f.toPath(), Charset.defaultCharset())
-        val program = Assembler.assemble(Assembler.parse(lines.toTypedArray()))
-        execute(program)
-    }
+private fun assembleAndExecute(f: File) {
+    val lines = Files.readAllLines(f.toPath(), Charset.defaultCharset())
+    val program = Assembler.assemble(Assembler.parse(lines.toTypedArray()))
+    execute(program)
+}
 
-    private fun execute(program: Array<Word32>) {
-        val p = Processor()
-        p.setProgram(program)
-        p.run()
-        println(p)
-    }
+private fun execute(program: Array<Word32>) {
+    val p = Processor()
+    p.setProgram(program)
+    p.run()
+    println(p)
 }
